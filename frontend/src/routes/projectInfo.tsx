d8736a46c96b7900f5f57943d0f80ca5acfab746
chaandchopra <chaand.chopra@invesco.com>
2025-07-16 19:33:00 +0530

    MUTATION

--- frontend/src/routes/projectInfo.tsx
+++ frontend/src/routes/projectInfo.tsx
@@ -1,12 +1,13 @@
 
 import { useLoaderData, useNavigate } from 'react-router-dom';
 import { ProjectDetailHeader, ProjectDetailsProgressBar, ProjectDetailsProjectInfo, ProjectDetailsSprintNotes } from '../components/projectDetails'
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { deleteProject, getProjectById, updateProject } from '../services/projectService';
 import  { EditModal } from '../components/EditModal';
 import { useState } from 'react';
 import { DeleteModal } from '../components/DeleteModal';
 import { projMetaData } from '../utils/constants';
+import { convertLength } from '@mui/material/styles/cssUtils';
 
 export const projectLoader = async ({ params }) => {
     const { id } = params;
@@ -15,41 +16,55 @@ export const projectLoader = async ({ params }) => {
 
 const ProjectInfo = () => {
     const projectId = useLoaderData();
-    const queryClient = useQueryClient();
     const { data, isLoading, error } = useQuery({
         queryKey: [`projects/${projectId}`],
         queryFn: () => getProjectById(projectId),
     });
+    const queryClient = useQueryClient();
     const [open, setOpen] = useState(false);
     const [openDelete, setOpenDelete] = useState(false);
     const [type, setType] = useState("");
     const navigate = useNavigate();
 
-    if (isLoading) return (<p>Loading...</p>);
-    if (error) return (<p>Error fetching projects {error.message}</p>);
     
-    const onSave = async (updated) => {
-        try {
-            await updateProject(updated);
+    const updateMutation = useMutation ({
+        mutationFn: updateProject,
+        onSuccess: (updated) => {
             queryClient.setQueryData([`projects/${projectId}`], (oldData:[]) => {
                 if (!oldData) return [];
-                return { ...oldData, data:  updated }; 
+                return { ...oldData, data:  updated.data }; 
             });
-            alert("Project updated successfully!");
-          } catch (error) {
-            console.error("Error updating project:", error);
-          }
-    }
-    const onDelete = async (updated) => {
-        try {
-            await deleteProject(updated);
-            data.data = updated;
-            navigate("/");
-            alert("Project updated successfully!");
-          } catch (error) {
-            console.error("Error updating project:", error);
-          }
-    }
+            alert("Project updated successfully!!!");
+        },
+        onError: (err) => {
+            alert("Err updating project");
+            console.log(err);
+        }
+    })
+
+    const deleteMutation = useMutation({
+        mutationFn: deleteProject,
+        onSuccess: (updated) => {
+            queryClient.setQueryData([`projects/${projectId}`], (oldData:[]) => {
+                if (!oldData) return [];
+                return { ...oldData, data:  updated.data }; 
+            });
+            alert("Project updated successfully!!!");
+            navigate("/")
+            alert("Project is deleted !!")
+        },
+        onError: (err) => {
+            alert("Err deleting project");
+            console.log(err);
+        }
+    })
+
+    const onSave = (updated: any) => updateMutation.mutate(updated);
+    const onDelete = (updated: any) => deleteMutation.mutate(updated);
+
+    if (isLoading) return (<p>Loading...</p>);
+    if (error) return (<p>Error fetching projects {error.message}</p>);
+
     return (
         <div className='p-4'>
             <ProjectDetailHeader setOpenDelete={setOpenDelete} projectInfo={data.data}/>
