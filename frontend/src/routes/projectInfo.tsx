1139f2ebd619ea721cf89923653de83289f857c8
chaandchopra <chaand.chopra@invesco.com>
2025-07-16 19:58:16 +0530

    fixing database data

--- frontend/src/routes/projectInfo.tsx
+++ frontend/src/routes/projectInfo.tsx
@@ -3,7 +3,7 @@ import { useLoaderData, useNavigate } from 'react-router-dom';
 import { ProjectDetailHeader, ProjectDetailsProgressBar, ProjectDetailsProjectInfo, ProjectDetailsSprintNotes } from '../components/projectDetails'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { deleteProject, getProjectById, updateProject } from '../services/projectService';
-import  { EditModal } from '../components/EditModal';
+import { EditModal } from '../components/EditModal';
 import { useState } from 'react';
 import { DeleteModal } from '../components/DeleteModal';
 import { projMetaData } from '../utils/constants';
@@ -26,13 +26,13 @@ const ProjectInfo = () => {
     const [type, setType] = useState("");
     const navigate = useNavigate();
 
-    
-    const updateMutation = useMutation ({
+
+    const updateMutation = useMutation({
         mutationFn: updateProject,
         onSuccess: (updated) => {
-            queryClient.setQueryData([`projects/${projectId}`], (oldData:[]) => {
+            queryClient.setQueryData([`projects/${projectId}`], (oldData: []) => {
                 if (!oldData) return [];
-                return { ...oldData, data:  updated.data }; 
+                return { ...oldData, data: updated.data };
             });
             alert("Project updated successfully!!!");
         },
@@ -42,46 +42,55 @@ const ProjectInfo = () => {
         }
     })
 
-    const deleteMutation = useMutation({
-        mutationFn: deleteProject,
-        onSuccess: (updated) => {
-            queryClient.setQueryData([`projects/${projectId}`], (oldData:[]) => {
-                if (!oldData) return [];
-                return { ...oldData, data:  updated.data }; 
-            });
-            navigate("/")
-            alert("Project is deleted !!")
-        },
-        onError: (err) => {
-            alert("Err deleting project");
-            console.log(err);
-        }
-    })
+    // const deleteMutation = useMutation({
+    //     mutationFn: deleteProject,
+    //     onSuccess: (updated) => {
+    //         queryClient.setQueryData([`projects/${projectId}`], (oldData:[]) => {
+    //             if (!oldData) return [];
+    //             return { ...oldData, data:  updated.data }; 
+    //         });
+    //         navigate("/");
+    //     },
+    //     onError: (err) => {
+    //         alert("Err deleting project");
+    //         console.log(err);
+    //     }
+    // })
 
     const onSave = (updated: any) => updateMutation.mutate(updated);
-    const onDelete = (updated: any) => deleteMutation.mutate(updated);
+    const onDelete = async (updated) => {
+        try {
+            await deleteProject(updated);
+            data.data = updated;
+            navigate("/");
+            alert("Project updated successfully!");
+        } catch (error) {
+            console.error("Error updating project:", error);
+        }
+    }
+    // const onDelete = (updated: any) => deleteMutation.mutate(updated);
 
     if (isLoading) return (<p>Loading...</p>);
     if (error) return (<p>Error fetching projects {error.message}</p>);
 
     return (
         <div className='p-4'>
-            <ProjectDetailHeader setOpenDelete={setOpenDelete} projectInfo={data.data}/>
-            <ProjectDetailsProgressBar projectInfo={{ ...data.data, ...projMetaData }} editOpen={setOpen} detailType={setType}/>
-            <ProjectDetailsSprintNotes projectInfo={data.data} editOpen={setOpen} detailType={setType}/>
+            <ProjectDetailHeader setOpenDelete={setOpenDelete} projectInfo={data.data} />
+            <ProjectDetailsProgressBar projectInfo={{ ...data.data, ...projMetaData }} editOpen={setOpen} detailType={setType} />
+            <ProjectDetailsSprintNotes projectInfo={data.data} editOpen={setOpen} detailType={setType} />
             <ProjectDetailsProjectInfo projectInfo={data.data} editOpen={setOpen} detailType={setType} />
             <EditModal
                 open={open}
                 onClose={() => setOpen(false)}
                 initialData={data.data}
-                onSave={(updated) => {onSave(updated)}}
+                onSave={(updated) => { onSave(updated) }}
                 type={type}
             />
             <DeleteModal
                 open={openDelete}
                 projId={data.data.proj_id}
                 onClose={() => setOpenDelete(false)}
-                onConfirm={(projId) => {onDelete(projId)}}
+                onConfirm={(projId) => { onDelete(projId) }}
             />
         </div>
     );
